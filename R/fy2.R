dba80a8af2664a4d2dd6946c7d82e177454f12eb
Michael Pawlus <pawlus@SCT-2516-Michael-Pawlus.local>
2018-08-17 18:15:55 -0700

    adding the Rd file and NAMESPACE update and fixing types

--- R/fy2.R
+++ R/fy2.R
@@ -3,7 +3,7 @@
 #' @description calculates the fiscal year based on an existing date
 #'
 #' @param date a date-time object
-#' @param fy_start_month a number represeting the starting month for the fiscal year (defaults to 7 -- "July")
+#' @param fy_start_month a number representing the starting month for the fiscal year (defaults to 7 -- "July")
 #'
 #' @return a numeric value of fiscal years
 #' @export
@@ -11,11 +11,11 @@
 fy2  <- function(date,fy_start_month=7) {
 
   if(!is.character(date)&!lubridate::is.Date(date)&!lubridate::is.POSIXct(date)&!lubridate::is.POSIXlt(date)) {
-    stop("Please use values formatted as dates or charcater strings with only numbers convertible to dates like '2017-07-01'")
+    stop("Please use values formatted as dates or character strings with only numbers convertible to dates like '2017-07-01'")
   }
 
   if(stringr::str_detect(date,"[:alpha:]")) {
-    stop("Please use values formatted as dates or charcater strings with only numbers convertible to dates like '2017-07-01'")
+    stop("Please use values formatted as dates or character strings with only numbers convertible to dates like '2017-07-01'")
   }
 
   if(!is.numeric(fy_start_month)|fy_start_month>12) {
