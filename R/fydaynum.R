298d792d82da18f826f5304ab5977d076fd1e9d0
majerus <rich.majerus@gmail.com>
2018-08-20 10:37:51 -0400

    added ytd argument and year-to-date functionality to fydaynum

--- R/fydaynum.R
+++ R/fydaynum.R
@@ -1,18 +1,33 @@
 #' Fiscal Year Day Number
 #'
 #' @param x a date to convert to a numeric day of the fiscal year. Note that x should be of class "Date".
+#' @param yet a logical vector. If true only values for dates that are year-to-date will be returned.
 #'
 #' @return a numeric value describing what day of the fiscal year x is.
 #' @export
 #'
-fydaynum  <- function(x) {
 
-    x  <- as.Date(x)
+fydaynum  <- function(x = Sys.Date(), ytd = TRUE){
 
-    fyx  <- fy(x)
+  x  <- as.Date(x)
 
-    thisfystart  <- lubridate::ymd(paste0(fyx-1, "-07-01" ))
+  fyx  <- fy(x)
 
-    as.numeric( x - thisfystart) + 1
+  x_fystart  <- lubridate::ymd(paste0(fyx-1, "-07-01" ))
+
+  x_day <- as.numeric( x - x_fystart) + 1
+
+  if(ytd == TRUE){
+
+    this_fystart <- lubridate::ymd(paste0(fy2()-1, "-07-01" ))
+
+    today_day <- as.numeric( Sys.Date() - this_fystart) + 1
+
+    x_day <- ifelse(x_day <= today_day, x_day, NA)
+
+    x_day
+  }
 
 }
+
+
