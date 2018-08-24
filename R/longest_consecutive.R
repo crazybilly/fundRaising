5d39e3df932badbdf94fd7a1e26a30800e4f8e3a
Jake Tolbert <crazybilly@gmail.com>
2018-08-24 08:35:40 -0500

    Tweak longest_consecutive(): move mode check to the top. Tweak typography for style and readability.

--- R/longest_consecutive.R
+++ R/longest_consecutive.R
@@ -3,8 +3,8 @@
 #' @description calculates the longest consecutive giving streak given giving details for constituents containing a fiscal year field
 #'
 #' @param data a data frame with donor ids and the fiscal year of gifts
-#' @param ids the column name that contains the constituent IDs
-#' @param fys the column name that contains the fiscal year for gifts. If not present, run fy() or fy2().
+#' @param ids an unquoted column name that contains the constituent IDs
+#' @param fys an unquote column name that contains the fiscal year for gifts. If not present, run fy() or fy2().
 #' @param fy_range the maximum consecutive giving streak to try to find. By default, this uses the maximum range given the fiscal years provided.
 #' @param mode options for the output mode. By default, "append" creates a new data frame with all provided columns and the consecutive giving years column. There are two other options. "just_consecutive" will provide a table of IDs and values representing the longest consecutive giving streak. "summary" provides a count of donors for every consecutive giving value.
 #'
@@ -19,11 +19,18 @@ longest_consecutive <- function(data,ids,fys,fy_range = NA, mode = "append") {
     stop("The first argument must be a data frame")
   }
 
+  ## check mode
+  if(!mode %in% c("just_consecutive","append","summary")){
+    stop('mode must be one of these three options: "just_consecutive","append","summary"')
+  }
+
+
   # function to see if consecutive numbers of length n exist
   improved.rle <- function(x, n, incr = 1L) {
     result <- rle(diff(as.integer(x)) == incr)
     any(result$lengths >= n-1L  & result$values)
   }
+
 
   # enquosurate the column names provided as function arguments
   id_col <- dplyr::enquo(ids)
@@ -32,10 +39,10 @@ longest_consecutive <- function(data,ids,fys,fy_range = NA, mode = "append") {
   # use the full possible fiscal year range by default
   if(is.na(fy_range)) {
     recent_year <- max(data[,dplyr::quo_name(fy_col)])
-    first_year <- min(data[,dplyr::quo_name(fy_col)])
-    fy_range <- (recent_year-first_year)+1
+    first_year  <- min(data[,dplyr::quo_name(fy_col)])
+    fy_range    <- (recent_year-first_year)+1
   } else {
-    fy_range <- fy_range
+    fy_range    <- fy_range
   }
 
   ## get the longest consecutive giving streak
@@ -47,17 +54,20 @@ longest_consecutive <- function(data,ids,fys,fy_range = NA, mode = "append") {
     dplyr::summarise(cons_count = sum(purrr::map_lgl(1:fy_range, ~improved.rle(!!fy_col,.)))) %>%
     ungroup()
 
-  if(mode == "just_consecutive"){
+  if(mode == "just_consecutive") {
     return(consecutive_giving)
   }
 
-  if(mode == "append"){
+
+  if(mode == "append") {
     # append the data to the data provided
     data <- data %>%
       dplyr::inner_join(consecutive_giving, by = quo_name(id_col))
+    return(data)
   }
 
-  if(mode == "summary"){
+
+  if(mode == "summary") {
     # summary table for all consecutive giving values
     consecutive_giving_summary <- consecutive_giving %>%
       dplyr::group_by(cons_count) %>%
@@ -67,7 +77,4 @@ longest_consecutive <- function(data,ids,fys,fy_range = NA, mode = "append") {
     return(consecutive_giving_summary)
   }
 
-  if(!mode %in% c("just_consecutive","append","summary")){
-    stop('mode must be one of these three options: "just_consecutive","append","summary"')
-  }
 }
