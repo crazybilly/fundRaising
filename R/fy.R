55a07c3b66c552ea64f8e615ed9d975f2072b826
Jake Tolbert <crazybilly@gmail.com>
2018-08-28 17:00:16 -0500

    Merge fy2() into fy().

--- R/fy.R
+++ R/fy.R
@@ -1,14 +1,27 @@
-#' Calculate the MU fiscal year
+#' Calculate the fiscal year based on any starting month
 #'
 #' @description calculates the fiscal year based on an existing date
 #'
-#' @param x a date-time object
+#' @param date a vector of date or date-times
+#' @param fy_start_month a number representing the starting month for the fiscal year (defaults to 7, ie. July)
 #'
-#' @return a numeric value of fiscal years
+#' @return a numeric vector of fiscal years with the length of the date argument
 #' @export
 #'
-fy  <- function(x) {
+fy  <- function(date = Sys.Date(), fy_start_month = 7 ) {
 
-  ifelse(lubridate::month(x) >= 7, lubridate::year(x) +1, lubridate::year(x) )
+  if(!is.character(date)&!lubridate::is.Date(date)&!lubridate::is.POSIXct(date)&!lubridate::is.POSIXlt(date)) {
+    stop("Please use values formatted as dates or character strings with only numbers convertible to dates like '2017-07-01'")
+  }
+
+  if(stringr::str_detect(date,"[:alpha:]")) {
+    stop("Please use values formatted as dates or character strings with only numbers convertible to dates like '2017-07-01'")
+  }
+
+  if(!is.numeric(fy_start_month)|fy_start_month>12) {
+    stop("The fiscal year start month must be a number between 1 and 12 for example 3 for March")
+  }
+
+  ifelse(lubridate::month(date) >= fy_start_month, lubridate::year(date) +1, lubridate::year(date) )
 
 }
